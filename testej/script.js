fd2d4de2592dd32a5fdaa4df094b84ab73b76b4d
Tavaos <160794983+Tavaos@users.noreply.github.com>
2024-03-20 20:31:23 -0300

    www

---
+++ testej/script.js
@@ -0,0 +1,12 @@
+//var nome = prompt("Escreva seu nome: ");
+//alert("Hello World!!! " + nome);
+
+function ligada(){
+    document.getElementById('lampada').
+    src='ligada.webp';
+}
+
+function desligada(){
+    document.getElementById('lampada').
+    src='desligada.webp';
+}
